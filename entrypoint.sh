0e3f04ad2ade160fbc44c64c386ac1e41ee69d5a
Azizul Hakimi Mohd Yussuf Izzudin <ahmyi@users.noreply.github.com>
2024-10-10 22:47:01 +0800

    Repair entrypoint.sh for wallet rpc

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
+SCALA_HOME="/home/scala/.scala"
+
 if [ "$SCALA_WALLET_DISABLED" = "false" ]; then
-	SCALA_ARGUMENTS="--trusted-daemon --non-interactive --log-level=1 --rpc-bind-port 11813 --rpc-bind-ip=0.0.0.0"
+	SCALA_ARGUMENTS="--trusted-daemon --non-interactive --confirm-external-bin --log-level=1 --rpc-bind-port 11813 --max-log-files 2 --rpc-bind-ip=0.0.0.0"
 	if [ "$SCALA_DAEMON_ADDRESS" = "" ]; then
-		SCALA_DAEMON_ADDRESS="node.scalaproject.io:11812";
+		SCALA_DAEMON_ADDRESS="scala_daemon:11812";
 	fi
-		chown -R scala:scala /wallet && \
+	
 	SCALA_ARGUMENTS="$SCALA_ARGUMENTS --daemon-address $SCALA_DAEMON_ADDRESS"
 
-	if [ "$SCALA_WALLET_DIRECTORY" = "" ]; then
-		SCALA_WALLET_DIRECTORY="/home/scala/wallet";
-	fi
+	SCALA_WALLET_DIRECTORY="/home/scala/wallet";
 
 	if [ ! -d "$SCALA_WALLET_DIRECTORY" ]; then
 	  mkdir -p $SCALA_WALLET_DIRECTORY;
 	  chown -R scala:scala $SCALA_WALLET_DIRECTORY
 	fi
 		
-	SCALA_ARGUMENTS="$SCALA_ARGUMENTS --wallet-dir $SCALA_WALLET_DIRECTORY"
+	SCALA_ARGUMENTS="$SCALA_ARGUMENTS --wallet-dir $SCALA_WALLET_DIRECTORY --log-file $SCALA_WALLET_DIRECTORY/logs/scala-wallet-rpc.log "
 
 	if [ "$SCALA_WALLET_RPC_LOGIN" != "" ]; then
 		SCALA_ARGUMENTS="$SCALA_ARGUMENTS --rpc-login $SCALA_WALLET_RPC_LOGIN"
@@ -28,10 +28,8 @@ if [ "$SCALA_WALLET_DISABLED" = "false" ]; then
 	if [ "$SCALA_WALLET_JSON_FILE" != "" ]; then
 		SCALA_ARGUMENTS="$SCALA_ARGUMENTS --generate-from-json $SCALA_WALLET_DIRECTORY/$SCALA_WALLET_JSON_FILE"
 	fi
-
 	/usr/local/bin/scala-wallet-rpc $SCALA_ARGUMENTS
 else
-	SCALA_HOME="/home/scala/.scala"
 	if [ ! -d "$SCALA_HOME" ]; then
 	  	mkdir -p "$SCALA_HOME"
 		chown -R scala:scala "$SCALA_HOME"
@@ -39,4 +37,5 @@ else
 	
 	/usr/local/bin/scalad --p2p-bind-ip=0.0.0.0 --p2p-bind-port=11811 --rpc-bind-ip=0.0.0.0 --rpc-bind-port=11812 --non-interactive --confirm-external-bind --no-zmq
 fi
+
 
